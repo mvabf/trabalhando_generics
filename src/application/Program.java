da19d73cf9dd444437702f0961c9dc252bd7f39e
Marcus Venicius <mvabf_dev@outlook.com>
2020-02-04 13:00:50 -0300

    generics delimitados

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,24 +1,46 @@
 package application;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
 
-import services.PrintService;
+import entities.Product;
+import services.CalculationService;
 
 public class Program {
-	public static void main(String[]args) {
-		
-		Scanner in = new Scanner(System.in);
-		PrintService<String> ps = new PrintService<>();
-		System.out.print("How many values? ");
-		int n = in.nextInt();
-		
-		for (int i = 0; i < n; i++) {
-			ps.addValue(in.next());
+	public static void main(String[] args) {
+
+		Locale.setDefault(Locale.US);
+
+		List<Product> list = new ArrayList<>();
+
+		String path = "c:\\temp\\in.txt";
+
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+
+			String line = br.readLine();
+
+			while (line != null) {
+
+				String[] fields = line.split(",");
+
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+
+				list.add(new Product(name, price));
+				line = br.readLine();
+			}
+
+			Product x = CalculationService.max(list);
+			System.out.println("Most Expensive: ");
+			System.out.println(x);
+
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
-		
-		ps.print();
-		System.out.println("First: " + ps.first());
-		
-		in.close();
+
 	}
 }
